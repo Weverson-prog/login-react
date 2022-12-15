3f5276b395ce455ecb25aae482a3281fc07f3a55
Weverson Santos de Oliveira Queiroz <74273665+Weverson-prog@users.noreply.github.com>
2022-12-15 10:30:45 -0300

    att token capture

--- src/context/AuthProvider/getDash.tsx
+++ src/context/AuthProvider/getDash.tsx
@@ -1,11 +1,8 @@
-import { useContext } from "react"
-import { AuthContext } from ".";
 import { Root } from "./types";
-import { LoginRequest, setUserLocalStorage, getUserLocalStorage } from './util';
+import { getUserLocalStorage } from './util';
 
-const token = JSON.stringify(Object.values(getUserLocalStorage())[0])
 export async function getCard() {
-  
+    const token = JSON.stringify(getUserLocalStorage().token)
     const response = await fetch('http://conciliador.sandbox.pratico.tech/dashboard/card?start_date=2022-10-01&final_date=2022-10-31', {
       method: 'get',
       headers: {
@@ -14,13 +11,6 @@ export async function getCard() {
       }})
     
       const dados : Root = await response.json();
-      
-      
-        //console.log(dados.card[0].sale_amount)
-        console.log(dados)
         
       return  dados ;
-
-      
-  
     }
